e09bb352093c56d4160a637df82b3a2e2bc4a7ad
abhi989023 <awasthiabhishek703@gmail.com>
2022-04-09 03:23:56 +0530

    Welcome java

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+package Linecomparision;
+
+public class Welcome {
+	
+
+	public static void main(String[] args) {
+	    System.out.println("Welcome to Line computation Program on Master Branch");
+	}
+
+}
